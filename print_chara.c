fdf4346b34615dcd8b8e6c70fa3cc713926a8f47
ElyasYid <140306064+ElyasYid@users.noreply.github.com>
2023-10-16 15:22:24 +0300

    Update print_chara.c

--- print_chara.c
+++ print_chara.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * printf_char - prints a char.
+ * prnt_ch - prints a char.
  * @val: arguments.
  * Return: 1.
  */
-int printf_char(va_list val)
+int prnt_ch(va_list val)
 {
 	char s;
 
