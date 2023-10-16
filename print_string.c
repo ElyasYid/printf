878085a38c6b315c083d31c6e43c50e1a886d42c
ElyasYid <140306064+ElyasYid@users.noreply.github.com>
2023-10-16 15:38:53 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * printf_string - print a string.
- * @val: argumen t.
+ * prnt_string - print a string.
+ * @val: argument.
  * Return: the length of the string.
  */
 
-int printf_string(va_list val)
+int prnt_string(va_list val)
 {
 	char *s;
 	int i, len;
