f788a7665197bfd5046baeb1c0b45c9b112a146f
ElyasYid <140306064+ElyasYid@users.noreply.github.com>
2023-10-16 15:26:04 +0300

    Update print_REVstr.c

--- print_REVstr.c
+++ print_REVstr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * printf_srev - function that prints a str in reverse
- * @args: type struct va_arg where is allocated printf arguments
- *
+ * prnt_strrev - function reverses string
+ * @args: arguments
  * Return: the string
  */
-int printf_srev(va_list args)
+int prnt_strrev(va_list args)
 {
 
 	char *s = va_arg(args, char*);
