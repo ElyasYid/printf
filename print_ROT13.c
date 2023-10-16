a0dfb6460152da7461784c8d4926c1a1e95d37c3
ElyasYid <140306064+ElyasYid@users.noreply.github.com>
2023-10-16 15:24:37 +0300

    Update print_ROT13.c

--- print_ROT13.c
+++ print_ROT13.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * printf_rot13 - printf str to ROT13 place into buffer
- * @args: type struct va_arg where is allocated printf arguments
+ * prnt_rot13 - prints strng to ROT13 place into buffer
+ * @args: arguments
  * Return: counter
  *
  */
-int printf_rot13(va_list args)
+int prnt_rot13(va_list args)
 {
 	int i, j, counter = 0;
 	int k = 0;
